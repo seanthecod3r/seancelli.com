415256a6a3ecb9028c720505691a30c4124059ba
seanthecod3r <sean.celli@yahoo.com>
2024-07-31 16:57:10 -0700

    Made main colors available app wide

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,19 @@ module.exports = {
 				'main-font':  ['Montserrat'],
 				'expertise': ['Arial', 'sans-serif'],
 			},
+			colors: {
+				'primary': '#0B3D91',
+				'secondary': '#ebeff3',
+			},
+			keyframes: {
+				slideIn: {
+					'0%': { transform: 'translateX(-100%)', opacity: '0'},
+					'100%': { transform: 'translateX(0)', opacity: '1'},
+				}
+			},
+			animation: {
+				slideIn: 'slideIn 1.5s ease-out forwards',
+			}
 		},
 	},
 	plugins: [],

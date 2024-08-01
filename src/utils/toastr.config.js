d7ca71d8c76d75cfa58a7f574d80b6f065f45d34
seanthecod3r <sean.celli@yahoo.com>
2024-07-31 22:45:58 -0700

    Greatly improved responsiveness and added real links

---
+++ src/utils/toastr.config.js
@@ -0,0 +1,11 @@
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
+
+toastr.options = {
+  closeButton: true,
+  progressBar: true,
+  positionClass: "toast-top-right",
+  timeOut: "5000",
+};
+
+export default toastr;
